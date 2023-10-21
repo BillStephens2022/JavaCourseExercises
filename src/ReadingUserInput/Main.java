c2c1a1e1a23038d0b15687c4fe78be28b6c983bd
BillStephens2022 <two4onebill@yahoo.com>
2023-10-21 08:22:57 -0400

    updated getInputFromScanner method

--- src/ReadingUserInput/Main.java
+++ src/ReadingUserInput/Main.java
@@ -1,5 +1,8 @@
 package ReadingUserInput;
 
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -20,12 +23,16 @@ public class Main {
 
         int currentYear = 2023;
 
-        System.out.println(getInputFromConsole(currentYear));
-        System.out.println(getInputFromScanner(currentYear));
+        try {
+            System.out.println(getInputFromConsole(currentYear));
+        } catch (NullPointerException e) {
+            System.out.println(getInputFromScanner(currentYear));
+        }
 
     }
 
     public static String getInputFromConsole(int currentYear) {
+
         String name = System.console().readLine("Hi, What's your Name? ");
         System.out.println("Hi " + name + ", Thanks for taking the course!");
 
@@ -36,7 +43,19 @@ public class Main {
     }
 
     public static String getInputFromScanner(int currentYear) {
+        Scanner scanner = new Scanner(System.in);
 
-        return "";
+        System.out.println("Hi, What's your Name? ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hi " + name + ", Thanks for taking the course!");
+
+        System.out.println("What year were you born? ");
+        String dateOfBirth = scanner.nextLine();
+
+        int age = currentYear - Integer.parseInt(dateOfBirth);
+
+        return "So you are " + age + " years old";
+
     }
 }
