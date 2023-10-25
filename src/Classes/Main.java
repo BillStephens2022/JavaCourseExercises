ba16bb10e25c576b88259c70af2b3a6c41d97294
BillStephens2022 <two4onebill@yahoo.com>
2023-10-25 07:47:04 -0400

    added SimpleCalculator class

--- src/Classes/Main.java
+++ src/Classes/Main.java
@@ -35,6 +35,16 @@ public class Main {
         System.out.println(myAccount.getAccountBalance());
         myAccount.withdrawFunds(5000000);
         System.out.println(myAccount.getAccountBalance());
+
+        SimpleCalculator calculator = new SimpleCalculator();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println("add= " + calculator.getAdditionResult());
+        System.out.println("subtract= " + calculator.getSubtractionResult());
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println("multiply= " + calculator.getMultiplicationResult());
+        System.out.println("divide= " + calculator.getDivisionResult());
     }
 
 
