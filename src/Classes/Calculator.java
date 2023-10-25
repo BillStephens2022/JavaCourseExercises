c435e182066202c1e1b114a9686144e021ab7337
BillStephens2022 <two4onebill@yahoo.com>
2023-10-25 13:56:28 -0400

    added Floor, Carpet, and Calculator classes

---
+++ src/Classes/Calculator.java
@@ -0,0 +1,15 @@
+package Classes;
+
+public class Calculator {
+    private Floor floor;
+    private Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet) {
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+
+    public double getTotalCost() {
+        return this.floor.getArea() * this.carpet.getCost();
+    }
+}
