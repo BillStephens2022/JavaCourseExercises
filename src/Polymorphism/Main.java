8713f9c00ff114bae842654377abd417e9c4cfcf
BillStephens2022 <two4onebill@yahoo.com>
2023-11-04 07:38:18 -0400

    added Movie class along with various subclasses

--- src/Polymorphism/Main.java
+++ src/Polymorphism/Main.java
@@ -1,8 +1,28 @@
 package Polymorphism;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Movie theMovie = new Adventure("Star Wars");
-        theMovie.watchMovie();
+        // note we can use "Science Fiction" or "Science" or "S" because the getMovie method
+        // looks at the first character of the type entered as a parameter to determine which
+        // type of Movie to create
+
+//        Movie theMovie = Movie.getMovie("Science Fiction", "Star Wars");
+//        theMovie.watchMovie();
+
+        Scanner s = new Scanner(System.in);
+        while (true) {
+            System.out.print("Enter Type (A for Adventure, C for Comedy, S for Science Fiction, or Q to quit): ");
+            String type = s.nextLine();
+            if ("Qq".contains(type)) {
+                break;
+            }
+            System.out.print("Enter Movie Title: ");
+            String title = s.nextLine();
+            Movie movie = Movie.getMovie(type, title);
+            movie.watchMovie();
+        }
+
     }
 }
