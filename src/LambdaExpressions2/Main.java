c73fe6cc42b6c2d2c9175fad33169eb4dedc0167
BillStephens2022 <two4onebill@yahoo.com>
2023-12-09 07:26:05 -0500

    added LambdaExpressions2 package with another example of a Lambda expression

---
+++ src/LambdaExpressions2/Main.java
@@ -0,0 +1,25 @@
+package LambdaExpressions2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>(List.of(
+                "alpha", "bravo", "charlie", "delta"
+        ));
+        for (String s : list) {
+            System.out.println(s);
+        }
+
+        System.out.println("----------");
+        list.forEach((s) -> System.out.println(s));
+
+        System.out.println("----------");
+        String prefix = "nato";
+        list.forEach((var myString) -> {
+            char first = myString.charAt(0);
+            System.out.println(prefix + " " + myString + " means " + first);
+        });
+    }
+}
