404517c75c62742595e36eb81138266ba2a4e0c8
BillStephens2022 <two4onebill@yahoo.com>
2023-12-31 08:43:52 -0500

    added 2 new packages for ImmutableClassesExternal and ImmutableClassesHacker to explore mutability and defensive copies

---
+++ src/ImmutableClassesHacker/PersonOfInterest.java
@@ -0,0 +1,13 @@
+package ImmutableClassesHacker;
+
+import ImmutableClasses.PersonImmutable;
+
+public class PersonOfInterest extends PersonImmutable {
+
+    public PersonOfInterest(PersonImmutable person) {
+        super(person);
+    }
+
+
+
+}
