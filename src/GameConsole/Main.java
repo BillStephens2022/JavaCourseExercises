f9d31656102ccc3cb9b39afe3bd31e41f5f03421
BillStephens2022 <two4onebill@yahoo.com>
2024-01-03 15:46:29 -0500

    added Pirate class and Weapons enum for the Pirate game

--- src/GameConsole/Main.java
+++ src/GameConsole/Main.java
@@ -1,14 +1,24 @@
 package GameConsole;
 
-import GameConsole.game.GameConsole;
-import GameConsole.game.ShooterGame;
+import GameConsole.pirate.Pirate;
+import GameConsole.pirate.Weapon;
 
 public class Main {
     public static void main(String[] args) {
 
-        var console = new GameConsole<>(new ShooterGame("The Shootout Game"));
-        int playerIndex = console.addPlayer();
-        console.playGame(playerIndex);
+//        var console = new GameConsole<>(new ShooterGame("The Shootout Game"));
+//        int playerIndex = console.addPlayer();
+//        console.playGame(playerIndex);
+
+        Weapon weapon = Weapon.getWeaponByChar('P');
+        System.out.println("weapon = " + weapon + ", hitPoints = " +
+                weapon.getHitPoints() + ", minLevel = " + weapon.getMinLevel());
+        var list = Weapon.getWeaponByLevel(1);
+        list.forEach(System.out::println);
+
+        Pirate tim = new Pirate("Tim");
+        System.out.println(tim);
+
 
     }
 }
