f67b4848b3b08073cd5ac8b0340c2316ceb63d7a
BillStephens2022 <two4onebill@yahoo.com>
2024-01-03 07:03:07 -0500

    added GameConsole class, Shooter record, and ShooterGame class

--- src/GameConsole/Main.java
+++ src/GameConsole/Main.java
@@ -1,7 +1,14 @@
 package GameConsole;
 
+import GameConsole.game.GameConsole;
+import GameConsole.game.ShooterGame;
+
 public class Main {
     public static void main(String[] args) {
+
+        var console = new GameConsole<>(new ShooterGame("The Shootout Game"));
+        int playerIndex = console.addPlayer();
+        console.playGame(playerIndex);
 
     }
 }
