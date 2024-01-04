6954fa784d4be7c902ef036c5c5b104cacdd7342
BillStephens2022 <two4onebill@yahoo.com>
2024-01-04 08:03:56 -0500

    added new functionality to Pirate Game to battle opponents, find loot, and have experiences in various townsJ

--- src/GameConsole/Main.java
+++ src/GameConsole/Main.java
@@ -1,9 +1,7 @@
 package GameConsole;
 
 import GameConsole.game.GameConsole;
-import GameConsole.pirate.Pirate;
 import GameConsole.pirate.PirateGame;
-import GameConsole.pirate.Weapon;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,18 +10,18 @@ public class Main {
 //        int playerIndex = console.addPlayer();
 //        console.playGame(playerIndex);
 
-        Weapon weapon = Weapon.getWeaponByChar('P');
-        System.out.println("weapon = " + weapon + ", hitPoints = " +
-                weapon.getHitPoints() + ", minLevel = " + weapon.getMinLevel());
-        var list = Weapon.getWeaponByLevel(1);
-        list.forEach(System.out::println);
-
-        Pirate tim = new Pirate("Tim");
-        System.out.println(tim);
-
-        PirateGame.getTowns(0).forEach(System.out::println);
-        System.out.println("-----------------------------------");
-        PirateGame.getTowns(1).forEach(System.out::println);
+//        Weapon weapon = Weapon.getWeaponByChar('P');
+//        System.out.println("weapon = " + weapon + ", hitPoints = " +
+//                weapon.getHitPoints() + ", minLevel = " + weapon.getMinLevel());
+//        var list = Weapon.getWeaponByLevel(1);
+//        list.forEach(System.out::println);
+//
+//        Pirate tim = new Pirate("Tim");
+//        System.out.println(tim);
+//
+//        PirateGame.getTowns(0).forEach(t -> System.out.println(t.information()));
+//        System.out.println("-----------------------------------");
+//        PirateGame.getTowns(1).forEach(t -> System.out.println(t.information()));
 
         var console = new GameConsole<>(new PirateGame("The Pirate Game"));
         int playerIndex = console.addPlayer();
