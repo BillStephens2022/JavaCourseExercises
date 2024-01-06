3cb9317a66eb6d642108d2419450d2a13fbf9807
BillStephens2022 <two4onebill@yahoo.com>
2024-01-06 09:05:10 -0500

    added more examples of intermediate stream operations like map, peek, sorted

--- src/StreamsIntermediate/Main.java
+++ src/StreamsIntermediate/Main.java
@@ -1,5 +1,6 @@
 package StreamsIntermediate;
 
+import java.util.Comparator;
 import java.util.Random;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -27,11 +28,27 @@ public class Main {
 
         System.out.println();
         Random random = new Random();
-        Stream.generate(() -> random.nextInt((int)'A', (int)'Z' + 1))
+        Stream.generate(() -> random.nextInt((int) 'A', (int) 'Z' + 1))
                 .limit(50)
                 .distinct()
                 .sorted()
                 .forEach(d -> System.out.printf("%c ", d));
+
+        System.out.println();
+        int maxSeats = 100;
+        int seatsInRow = 10;
+        var stream = Stream.iterate(0, i -> i < maxSeats, i -> i + 1)
+                .map(i -> new Seat((char) ('A' + i / seatsInRow),
+                        i % seatsInRow + 1))
+                .skip(5)
+                .limit(10)
+                .peek(s -> System.out.println("--> " + s))
+                        .sorted(Comparator.comparing(Seat::price)
+                                .thenComparing(Seat::toString));
+//                .mapToDouble(Seat::price)
+//                .boxed()
+//                .map("%.2f"::formatted);
+        stream.forEach(System.out::println);
 
     }
 }
