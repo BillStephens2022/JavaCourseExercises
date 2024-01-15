c48e5831ed98b362bfbf361c031600498133e7f1
BillStephens2022 <two4onebill@yahoo.com>
2024-01-15 07:36:18 -0500

    added files folder and testing.csv file, added example of checking if file exists by using the file path

--- src/FileExceptions/Main.java
+++ src/FileExceptions/Main.java
@@ -4,22 +4,33 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
-        String filename = "testing.csv";
-
-        testFile2(null);
+        System.out.println("Current Working Directory (cwd) = " +
+                new File("").getAbsolutePath());
+        String filename = "files/testing.csv";
 
         File file = new File(filename);
         if (!file.exists()) {
             System.out.println("I can't run unless this file exists");
-            System.out.println("Quitting Application, go figure it out");
             return;
         }
         System.out.println("I'm good to go.");
+        for (File f : File.listRoots()) {
+            System.out.println(f);
+        }
+
+        Path path = Paths.get("files/testing.csv");
+        System.out.println(file.getAbsolutePath());
+        if (!Files.exists(path)) {
+            System.out.println("2. I can't run unless this file exists");
+            return;
+        }
+        System.out.println("2. I'm good to go.");
     }
 
     private static void testFile(String filename) {
