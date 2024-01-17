8058ae319cd6b74ac38a71161a188e06a963de10
BillStephens2022 <two4onebill@yahoo.com>
2024-01-17 06:16:43 -0500

    added examples to WritingFiles package using BufferedWriter, FileWriter, and PrintWriter classes

--- src/Files/WritingFiles/Main.java
+++ src/Files/WritingFiles/Main.java
@@ -4,10 +4,11 @@ import Files.WritingFiles.student.Course;
 import Files.WritingFiles.student.Student;
 
 import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -23,7 +24,7 @@ public class Main {
         Course pymc = new Course("PYC", "Python Masterclass");
         List<Student> students = Stream
                 .generate(() -> Student.getRandomStudent(jmc, pymc))
-                .limit(5)
+                .limit(25)
                 .toList();
 //        System.out.println(header);
 //        students.forEach(s -> s.getEngagementRecords().forEach(System.out::println));
@@ -38,25 +39,82 @@ public class Main {
 //            e.printStackTrace();
 //        }
 
-        try {
-            List<String> data = new ArrayList<>();
-            data.add(header);
-            for (Student student : students) {
-                data.addAll(student.getEngagementRecords());
-            }
-            Files.write(path, data);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+//        try {
+//            List<String> data = new ArrayList<>();
+//            data.add(header);
+//            for (Student student : students) {
+//                data.addAll(student.getEngagementRecords());
+//            }
+//            Files.write(path, data);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        // BufferedWriter
         try (BufferedWriter writer =
                 Files.newBufferedWriter(Path.of("take2.csv"))) {
             writer.write(header);
             writer.newLine();
+            int count = 0;
             for (Student student : students) {
                 for (var record : student.getEngagementRecords()) {
                     writer.write(record);
                     writer.newLine();
+                    count++;
+                    if (count % 5 == 0) {
+                        Thread.sleep(2000);
+                        System.out.print(".");
+                    }
+                    if (count % 10 == 0) {
+                        writer.flush();
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // FileWriter
+        try (FileWriter writer =
+                     new FileWriter("take3.csv")) {
+            writer.write(header);
+            writer.write(System.lineSeparator());
+            for (Student student : students) {
+                for (var record : student.getEngagementRecords()) {
+                    writer.write(record);
+                    writer.write(System.lineSeparator());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // PrintWriter
+        try (PrintWriter writer =
+                     new PrintWriter("take4.txt")) {
+            writer.println(header);
+
+            for (Student student : students) {
+                for (var record : student.getEngagementRecords()) {
+                    String[] recordData = record.split(",");
+                    writer.printf("%-12d%-5s%2d%4d%3d%-1s".formatted(
+                            student.getStudentId(), // Student Id
+                            student.getCountry(), // Country Code
+                            student.getEnrollmentYear(), // Enrolled Year
+                            student.getEnrollmentMonth(), // Enrolled Month
+                            student.getEnrollmentAge(), // Age
+                            student.getGender() // Gender
+                    ));
+                    writer.printf("%-1s",
+                            (student.hasExperience() ? 'Y' : 'N')); // Experienced ?
+                    writer.format("%-3s%10.2f%-10s%-4s%-30s",
+                            recordData[7], //Course Code
+                            student.getPercentComplete(recordData[7]),
+                            recordData[8], // Engagement Month
+                            recordData[9], // Engagement Year
+                            recordData[10]); // Engagement Type
+                    writer.println();
                 }
             }
         } catch (IOException e) {
