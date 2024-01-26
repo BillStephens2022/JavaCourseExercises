8f2be5f18520122cc20d24544af274c5e039305e
BillStephens2022 <two4onebill@yahoo.com>
2024-01-26 14:08:48 -0500

    implemented some example tests including a parameterized test

--- src/Testing/BankAccountTest.java
+++ src/Testing/BankAccountTest.java
@@ -4,38 +4,64 @@ import static org.junit.Assert.*;
 
 public class BankAccountTest {
 
+    private BankAccount account;
+    private static int count;
+
+    @org.junit.BeforeClass
+    public static void beforeClass() {
+        System.out.println("This executes before any test cases. Count = " + count++);
+    }
+
+    @org.junit.Before
+    public void setup() {
+        account = new BankAccount("Bill", "Stephens", 1000, BankAccount.CHECKING);
+        System.out.println("Running a test...");
+    }
+
     @org.junit.Test
     public void deposit() throws Exception {
-        BankAccount account = new BankAccount("Bill", "Stephens", 1000, BankAccount.CHECKING);
         double balance = account.deposit(200.00, true);
         assertEquals(1200, balance, 0);
     }
 
     @org.junit.Test
-    public void withdraw() throws Exception {
-        BankAccount account = new BankAccount("Bill", "Stephens", 1000, BankAccount.CHECKING);
-        double balance = account.withdraw(200.00, true);
-        assertEquals(800, balance, 0);
+    public void withdraw_branch() throws Exception {
+        double balance = account.withdraw(600.00, true);
+        assertEquals(400.00, balance, 0);
+    }
+
+    @org.junit.Test(expected = IllegalArgumentException.class)
+    public void withdraw_notBranch() throws Exception {
+        account.withdraw(600.00, false);
+        fail("Should have thrown an IllegalArgumentException");
     }
 
     @org.junit.Test
     public void getBalance_deposit() throws Exception {
-        BankAccount account = new BankAccount("Bill", "Stephens", 1000, BankAccount.CHECKING);
         account.deposit(200.00, true);
         assertEquals(1200, account.getBalance(), 0);
     }
 
     @org.junit.Test
-    public void getBalance_widthdraw() throws Exception {
-        BankAccount account = new BankAccount("Bill", "Stephens", 1000, BankAccount.CHECKING);
+    public void getBalance_withdraw() throws Exception {
         account.withdraw(200.00, true);
         assertEquals(800.00, account.getBalance(), 0);
     }
 
     @org.junit.Test
     public void isChecking_true() {
-        BankAccount account = new BankAccount("Bill", "Stephens", 1000, BankAccount.CHECKING);
         assertTrue(account.isChecking());
     }
+
+    @org.junit.AfterClass
+    public static void afterClass() {
+        System.out.println("This executes after any test cases. Count = " + count++);
+    }
+
+    @org.junit.After
+    public void teardown() {
+        System.out.println("Count = " + count++);
+    }
+
 
 }
